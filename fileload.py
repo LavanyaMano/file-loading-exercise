79afed735254ce37dfb56bae6e645ab88ff19e33
lava m <lavanymkanna@gmail.com>
2016-09-03 13:29:19 -0400

    class and methods added

--- fileload.py
+++ fileload.py
@@ -2,65 +2,77 @@ import json
 with open('store_data.json', 'r') as data_file:
     data = json.loads(data_file.read())
 
+class Fileload():
 
-price =[]
-cost =[]
-sold =[]
-totalrev = 0
-totalcost= 0
-dept =[]
-dept_sold=[]
-eachdept = 0
+	def __init__(self):
+		self.price =[]
+		self.cost =[]
+		self.sold =[]
+		self.totalrev = 0
+		self.totalcost= 0
+		self.dept =[]
+		self.dept_sold=[]
+		self.eachdept = 0
+
+	def listings(self):
+		for item in data:
+			self.price.append(item["price"])
+			self.cost.append(item["cost_to_make"])
+			self.sold.append(item["sold"])
+			if item["department"] not in self.dept:
+				self.dept.append(item["department"])
+		return (self.price,self.cost,self.sold,self.dept)
+
+	def sales(self):
+		for i in data:
+			self.totalrev += i["price"] * i["sold"]
+			self.totalcost += i["cost_to_make"] * i["sold"]
+		print("Total revenue is ${:.2f}".format(self.totalrev))
+		print("Total profit is ${:.2f}".format(self.totalrev-self.totalcost))
+		
+
+	def sales_deptwise(self):
+		print("Number sold by each department :\n")
+		for item in data:
+					for i in self.dept:
+						if i== item["department"]:
+							self.eachdept+= item["sold"]
+					self.dept_sold.append(self.eachdept)
+		for i in range(len(dept)):
+			print("{} : {}nos".format(self.dept[i],self.dept_sold[i]))
+
+	def best_dept(self):
+		new_list = list(reversed(sorted(data,key=lambda k: k["sold"])))
+		print("Top 10 best sellers are: \n")
+
+		for i in range(10):
+			print(new_list[i]["name"],":")
+			print(new_list[i]["sold"])
+
+	def high_price(self):
+		self.listings()
+		h=self.price.index(max(self.price))
+		print("The most expensive product is {}: ${:.2f}".format(data[h]["name"], max(self.price)))
+
+	def low_price(self):
+		self.listings()
+		l=self.price.index(min(self.price))
+		print("The least expensive product is {}: ${:.2f}".format(data[l]["name"], min(self.price)))
 
 
+store = Fileload()
 
-
-for item in data:
-	price.append(item["price"])
-	cost.append(item["cost_to_make"])
-	sold.append(item["sold"])
-	if item["department"] not in dept:
-		dept.append(item["department"])
-
-for i in data:
-	totalrev += i["price"] * i["sold"]
-	totalcost += i["cost_to_make"] * i["sold"]
-
-for item in data:
-	for i in dept:
-		if i== item["department"]:
-			eachdept+= item["sold"]
-	dept_sold.append(eachdept)
-
+print("-"*30)
+store.sales()
 
 print("-"*30)
 
-print("Number sold by each department :\n")
-
-for i in range(len(dept)):
-	print("{} : {}nos".format(dept[i],dept_sold[i]))
-
-
-h=price.index(max(price))
-l=price.index(min(price))
+store.high_price()
+store.low_price()
+print("-"*30)
+store.best_dept()
 
 print("-"*30)
 
-print("Total revenue is ${:.2f}".format(totalrev))
 
-print("Total profit is ${:.2f}".format(totalrev-totalcost))
-print("-"*30)
-
-print("The most expensive product is {}: ${:.2f}".format(data[h]["name"], max(price)))
-
-print("The least expensive product is {}: ${:.2f}".format(data[l]["name"], min(price)))
-
-print("-"*30)
-
-new_list = list(reversed(sorted(data,key=lambda k: k["sold"])))
-print("Top 10 best sellers are: \n")
-
-for i in range(10):
-	print(new_list[i]["name"],":")
-	print(new_list[i]["sold"])
 
